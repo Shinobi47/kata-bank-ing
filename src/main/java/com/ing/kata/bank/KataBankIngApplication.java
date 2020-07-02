03f08c3d73cac1db51edc6f2943bd64bc83660d5
Shinobi47 <HashiramaSenju47@gmail.com>
2020-07-02 20:20:04 +0200

    Initial commit

---
+++ src/main/java/com/ing/kata/bank/KataBankIngApplication.java
@@ -0,0 +1,13 @@
+package com.ing.kata.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBankIngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataBankIngApplication.class, args);
+	}
+
+}
